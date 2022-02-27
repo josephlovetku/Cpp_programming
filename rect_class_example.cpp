dda7d6642e79c3a13b3c87185fd82c6ccccba7e2
josephlovetku <91767797+josephlovetku@users.noreply.github.com>
2022-02-27 22:25:58 +0800

    Add files via upload

---
+++ rect_class_example.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+class rectangle {
+public:
+	typedef int unit;
+  	void area();  	
+	void set(unit wd, unit ht);  	
+private:
+	unit width;
+	unit height;
+};
+
+void rectangle::set(unit wd, unit ht) 
+{
+	width = wd;
+	height = ht;
+}
+
+
+void rectangle::area() //defining function using scope resolution operator
+{
+  cout << "The area of the rectangle is: " << width * height << endl;
+}
+
+int main()
+{
+  rectangle obj; //creating object of rectangle class
+  rectangle::unit x, y;
+  cout << "Enter width: "; 
+  cin >> x;
+  cout << "Enter height: ";
+  cin >> y;
+  obj.set(x, y);
+  obj.area(); 
+  return 0;
+}
